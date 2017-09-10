bee370375f53fc3f2f6e64a2417f64f48fa89965
Len <Lenster921@gmail.com>
2017-09-10 01:47:37 -0700

    played with bittrex api examples

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 var serverUrl = process.argv[2] || 'localhost';
 var startup = require('./server/startup.js');
-startup.initialize(serverUrl);
+var fs = require('fs');
+var bittrex = require('node.bittrex.api');
+
+var _loadApiKeys = function() {
+  return new Promise(function (resolve, reject){
+   fs.readFile('api-keys.json', function(err, data) {
+      if (err) throw err;
+      resolve(JSON.parse(data));
+    });
+  });
+ };
+
+startup.initialize(serverUrl)
+.then(_loadApiKeys)
+.then(function(keys){
+  bittrex.options(keys);  
+  bittrex.getbalances( function( data, err ) {
+    console.log( data );
+  });
+});
